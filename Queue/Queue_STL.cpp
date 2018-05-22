262ef0170c0611607bbc15f578d093efba2c60fa
ScaryLittleOne <ngToan1107@gmail.com>
2018-05-22 13:54:17 +0700

    Modified Repo Arrangement

---
+++ Queue/Queue_STL.cpp
@@ -0,0 +1,20 @@
+﻿#include <queue>
+#include <iostream>
+using namespace std;
+
+int main(){
+	queue <int> q;
+	int n;
+	cin >> n;
+	int a[n];
+	for (int i = 0; i < n ; i++){
+		cin >> a[i];
+		q.push(a[i]);
+	}
+
+	while (!q.empty()){
+		cout << q.front() << " ";
+		q.pop();
+	}
+	return 0;
+}
---
+++ Stack/Stack_STL.cpp
@@ -0,0 +1,20 @@
+﻿#include <stack>
+#include <iostream>
+using namespace std;
+
+int main(){
+	stack <int> s;
+	int n;
+	cin >> n;
+	int a[n];
+	for (int i = 0; i < n ; i++){
+		cin >> a[i];
+		s.push(a[i]);
+	}
+
+	while (!s.empty()){
+		cout << s.top() << " ";
+		s.pop();
+	}
+	return 0;
+}
