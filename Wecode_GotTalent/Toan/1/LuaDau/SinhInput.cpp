f26fcdb2a85f668b9aeef26b1d7084b71df38f9b
ToanNguyen1107 <ngtoan1107@gmail.com>
2018-11-21 22:39:16 +0700

    Add a problem

---
+++ Wecode_GotTalent/Toan/1/LuaDau/SinhInput.cpp
@@ -0,0 +1,40 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long ld;
+#define FOR(i,a,b)\
+	for (auto i = a; i != b; i++)
+#define fi first
+#define se second
+string to_string(ld x){
+    char piBuffer[8];
+    sprintf(piBuffer, "%d", x);
+    string s(piBuffer);
+    return s;
+}
+int main(){
+	srand(time(NULL));
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	ld N = 20;
+	ll Lim = 1e12;//45000;
+	ofstream f;
+	FOR(t,0,N){
+		string s = "input" + to_string(t + 1) + ".txt";
+		//cout << s << endl;
+		f.open (s, ios::out | ios::app | ios::binary);
+
+		if (t <= 5){
+			f << (rand()%100 + 3);
+		}
+		else {
+		 	if (t <= 10){
+		 		f << (rand()%ld(sqrt(Lim)) + 3);
+		 	}
+		 	else
+		 		f << (Lim - t * 2);
+		}
+  		f.close();
+	}
+	return 0;
+}
---
+++ Wecode_GotTalent/Toan/1/LuaDau/SinhOutput.cpp
@@ -0,0 +1,42 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long ld;
+#define FOR(i,a,b)\
+	for (auto i = a; i != b; i++)
+#define fi first
+#define se second
+
+void Gen(ifstream &in,ofstream &f){
+	//Cap Nhat De Bai Yeu Cau Xuat Day Co So Phan Tu Nho Hon Truoc
+	ll n;
+	ll res = 0;
+	in >> n;
+	if (n <= 2){
+		f << "No";
+		return;
+	}
+	if (n % 2  == 0){
+		res = n / 2;	
+	}
+	else res = (n + 1) / 2;
+	f << "Yes\n1 " << res;
+}
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	ld N = 20;
+	ifstream in;
+	ofstream f;
+	FOR(t,1,N + 1){
+		string s = "input" + to_string(t) + ".txt";
+		string o = "output" + to_string(t) + ".txt";
+		in.open (s, ios::in | ios::app | ios::binary); 
+		f.open (o, ios::out | ios::app | ios::binary); 
+		Gen(in,f);
+		f.close();
+		in.close();
+	}
+	return 0;
+}
