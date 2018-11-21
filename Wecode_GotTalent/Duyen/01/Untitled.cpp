8040821d7074896e5b5bd545b35ed0c39d9db8d4
Le Nguyen Ky Duyen <duyenbit.2212@gmail.com>
2018-11-21 21:30:42 +0700

    New Even

---
+++ Wecode_GotTalent/Duyen/01/Untitled.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n,m;
+	cin>>n;
+	cin>>m;
+	int *a=new int[n];
+	for(int i=0;i<n;i++)
+	{
+		int p;
+		cin>>p;
+		a[i]=p;
+	}
+	for(int i=0;i<n-1;i++)
+	for(int j=i+1;j<n;j++)
+	if(a[i]>a[j])
+	{
+		int temp=a[i];
+		a[i]=a[j];
+		a[j]=temp;
+		}
+	
+	int iMax=0, iMin=0;
+	iMax=a[n-1]+m;
+
+	for(;m>0;m--)
+		{
+		a[0]=a[0]+1;
+		for(int i=0;i<n-1;i++)
+	for(int j=i+1;j<n;j++)
+	if(a[i]>a[j])
+	{
+		int temp=a[i];
+		a[i]=a[j];
+		a[j]=temp;
+		}
+		}
+	
+		iMin=a[n-1];
+	cout<<iMin<<" "<<iMax;
+	//	for(int i=0;i<n;i++)
+	//	cout<<a[i]<<" ";
+	return 0;
+}
