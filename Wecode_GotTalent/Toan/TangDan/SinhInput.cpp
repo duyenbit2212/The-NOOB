6b6edfb303d1532289ebeb3cc9d7ca6a9350e67b
ToanNguyen1107 <ngtoan1107@gmail.com>
2018-12-12 03:07:29 +0700

    Add a new Prob

---
+++ Wecode_GotTalent/Toan/TangDan/SinhInput.cpp
@@ -0,0 +1,47 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long ld;
+#define FOR(i,a,b)\
+	for (auto i = a; i != b; i++)
+#define fi first
+#define se second
+string to_string(ld x){
+    char piBuffer[8];
+    sprintf(piBuffer, "%d", x);
+    string s(piBuffer);
+    return s;
+}
+
+ll LimN = 2000;
+ll LimA = 1e5 + 1;
+void Gen(ofstream &out,ld t){
+	ld n;
+	if (t < 10) 
+		n = rand() % (LimN / (20 - t - 1)) + LimN / (20 - t - 1);
+	if (t >= 10){
+		n = LimN - (20 - t - 1);	
+	}
+	if (t <= 5){
+		n = t + rand() % 20;
+	}
+	out << n << "\n";             
+	FOR(i,0,n){
+		ld x = rand() % LimA;
+		out << x << " "; 
+	}
+}
+int main(){
+	srand(time(NULL));
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	ld N = 20;   
+	ofstream f;
+	FOR(t,0,N){
+		string s = "input" + to_string(t + 1) + ".txt";
+		f.open (s, ios::out | ios::app | ios::binary);
+		Gen(f,t);
+  		f.close();
+	}
+	return 0;
+}
---
+++ Wecode_GotTalent/Toan/TangDan/tester.cpp
@@ -0,0 +1,64 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long ld;
+#define FOR(i,a,b)\
+	for (auto i = a; i != b; i++)
+#define fi first
+#define se second
+
+int main(int argc, char const *argv[])
+{
+ 
+	ifstream test_in(argv[1]);    /* This stream reads from test's input file   */
+	ifstream test_out(argv[2]);   /* This stream reads from test's output file  */
+	ifstream user_out(argv[3]);   /* This stream reads from user's output file  */
+    
+    vector <ll> a;
+    ld n,m;
+    vector <ll> s;
+    vector <pair<ld,ll>> q;
+    test_in >> n;
+    a.resize(n + 2,0);
+    s.resize(n + 2,0);
+    FOR(i,1,n + 1){
+    	test_in >> a[i];
+    }
+    user_out >> m;
+    if (m > n + 1) 
+    	return 1;
+    FOR(it,0,m){
+    	ld t,i;
+    	ll x;
+    	user_out >> t >> i >> x;
+    	if (i > n || x > 1e6 || x < 0)
+    		return 1;
+    	if (t == 2){
+    		if (x == 0) 
+    			return 1;
+    		q.push_back({i,x});
+    		continue;
+    	}
+    	s[0] += x;
+    	s[i + 1] -= x;
+    }
+    FOR(i,1,n + 1){  
+    	s[i] += s[i - 1];
+    	a[i] += s[i];
+    }
+    for (auto v : q){
+    	ld k = v.fi;
+    	ll x = v.se;
+    	FOR(i,1,k + 1){
+    		s[i] %= x;
+    	}	
+    }
+    FOR(i,2,n + 1){
+    	if (s[i] <= s[i - 1]) 
+    		return 1; 	
+    }
+    return 0;
+	/* If user's output is correct, return 0, otherwise return 1       */
+	/* e.g.: Here the problem is: read n numbers and print their sum:  */
+ 
+}
