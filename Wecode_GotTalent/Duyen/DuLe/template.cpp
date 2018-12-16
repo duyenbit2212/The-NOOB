fd1704cc389b071679d8c2f16ce0788b989d6795
ToanNguyen1107 <ngtoan1107@gmail.com>
2018-12-17 02:23:14 +0700

    Fix problem Dule

---
+++ Wecode_GotTalent/Duyen/DuLe/template.cpp
@@ -0,0 +1,14 @@
+/*###Begin banned keyword - each of the following line if appear in code will raise error. regex supported
+define
+include
+class
+[
+]
+calloc
+malloc
+new
+###End banned keyword*/
+#include <iostream>
+#include <math.h>
+//###INSERT CODE HERE -
+                           
