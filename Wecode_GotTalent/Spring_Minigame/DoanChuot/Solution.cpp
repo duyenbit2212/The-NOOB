f65452db7352c2cf7c33d76a7bfabbe062dede13
Le Nguyen Ky Duyen <duyenbit.2212@gmail.com>
2019-02-11 22:10:15 +0700

    Update Solution.cpp

--- Wecode_GotTalent/Spring_Minigame/DoanChuot/Solution.cpp
+++ Wecode_GotTalent/Spring_Minigame/DoanChuot/Solution.cpp
@@ -10,15 +10,11 @@ int main() {
 		cin>>arr[i];
 
 	}
-	for(int i=1;i<n-1;i++){
-		if((arr[i]>arr[i-1])&&(arr[i]>arr[i+1])){
-			++count;
-		}
-		if(arr[i]<arr[i-1]&&arr[i]<arr[i+1]){
-					++count;
-				}
-
-	}
+	for (int i=1;i<n-1;i++)
+    {
+        if(((a[i]>a[i-1])&& (a[i]>a[i+1]))||((a[i]<a[i-1])&& (a[i]<a[i+1])))
+        count++;
+    }
 	cout<<count;
 
 	return 0;
